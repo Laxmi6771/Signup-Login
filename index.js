221b4a475a6416f0f7a08fe644bbbe7b511a0985
unknown <Laxmi6771>
2025-11-14 22:59:06 +0530

    Add Login function

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function signup(userName) {
 
 // Example usage
 console.log(signup("bob"));     // User Already Registered
-console.log(signup("manogna")); // Signup Successful
+console.log(signup("Laxmi")); // Signup Successful
+
+function login(userName, password) {
+  let users = ["alice", "bob", "charlie"];
+  if (!users.includes(userName)) {
+    return "User Not Found, Please Signup";
+  } else if (password === "Emp@123") {
+    return "Login Successful...";
+  } else {
+    return "Wrong Password....";
+  }
+}
+
+// Example usage
+console.log(login("bob", "Emp@123")); // Login Successful
+console.log(login("bob", "wrong"));   // Wrong Password
+console.log(login("Laxmi", "Emp@123")); // User Not Found
